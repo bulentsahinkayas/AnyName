ffb2c8be8e5e40018b2d318a31f1413a860c374a
bulentsahinkayas <bsahinkayasi58@gmail.com>
2020-12-08 18:39:38 -0500

    I added print smt

--- src/study/techno/AnotherClass.java
+++ src/study/techno/AnotherClass.java
@@ -3,5 +3,6 @@ package study.techno;
 public class AnotherClass {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("I added this");
     }
 }
